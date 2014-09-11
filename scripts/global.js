9e3821cb98c88d40e9c0516d16d2d86852ef85cd
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-09-11 20:20:25 +0200

    avec bootstrap

---
+++ scripts/global.js
@@ -0,0 +1,10 @@
+// Custom JS
+$(document).ready(function(){
+  $('body').flowtype({
+   minimum   : 500,
+   maximum   : 1200,
+   minFont   : 12,
+   maxFont   : 16,
+   fontRatio : 30
+  }); 
+});
